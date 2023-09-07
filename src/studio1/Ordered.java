8bd0ff11035a3e5b845ebe8bcaa255bfa450231a
drewwendler <drewwendler@localhost>
2023-09-07 12:49:32 -0500

    Studio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,15 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered = true;
+		if((x > y >> z) || (x < y << z))
+		{
+			System.out.print(isOrdered);
+		}else {
+		System.out.print(false);
+	}	
+			
+		
 	}
 
 }
